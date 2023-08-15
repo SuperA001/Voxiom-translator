9069f56ef7f3b4241671ff24214391f2d0881583
SuperA001 <119860090+SuperA001@users.noreply.github.com>
2023-08-15 20:38:54 +0300

    update script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function updateTranslations(translations) {
+    var elements = document.querySelectorAll('[class]')
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i]
+        var classTranslations = translations[element.getAttribute('class')]
+        if (!classTranslations) {
+            continue
+        }
+        for (var key in classTranslations) {
+            if (classTranslations.hasOwnProperty(key)) {
+                var value = classTranslations[key]
+                if (element.textContent.trim() === key) {
+                    element.textContent = value
+                }
+            }
+        }
+    }
+    let mainTitle = document.querySelectorAll('.sc-hiCibw.igNAJT > div')
+    for (var m = 0; m < mainTitle.length; m++) {
+        var mainTitleTranslations = translations.MainTitles
+        if (mainTitleTranslations.hasOwnProperty(mainTitle[m].textContent)) {
+            mainTitle[m].innerText = mainTitleTranslations[mainTitle[m].textContent]
+        }
+    }
+}
+
+function loadTranslations() {
+    fetch('https://raw.githubusercontent.com/TheMasterRob4ig/Voxiom.ioTranslated/main/tr2.json')
+        .then(response => response.json())
+        .then(data => updateTranslations(data.translations))
+}
+
+setInterval(loadTranslations, 100)
