ec4990d715e4aa70dd3f5e03fa71b09517dbaa09
SuperA001 <119860090+SuperA001@users.noreply.github.com>
2023-08-25 18:40:42 +0300

    +

---
+++ main.js
@@ -0,0 +1,17 @@
+//VoxiomTranslated
+fetch("https://raw.githubusercontent.com/SuperA001/Voxiom-translator/main/script.js ")
+    .then(response => response.text())
+    .then(data => {
+        const sriptElement = document.createElement("script")
+        sriptElement.textContent = data
+        document.head.appendChild(sriptElement)
+    })
+    
+//VoxiomModernStyle
+fetch("https://raw.githubusercontent.com/TheMasterRob4ig/VoxiomModernStyle/main/style.css")
+    .then(response => response.text())
+    .then(data => {
+        const styleElement = document.createElement("style")
+        styleElement.textContent = data
+        document.head.appendChild(styleElement)
+    })
